98c7ce5e82e5653d1655d94c0b5cfce81e11b423
Anuj Jha <anujjha2106@gmail.com>
2024-04-20 22:33:53 +0530

    Variables

---
+++ A03Varibale.py
@@ -0,0 +1,40 @@
+x = 10
+print(x)
+
+num1 = 10
+num2 = 20 
+sum = num1 + num2 
+print(sum)
+
+first_name = "Anuj"
+last_name = "Jha"
+
+print(first_name+" "+last_name)
+print(first_name+" Kumar "+last_name)
+
+print(first_name[0])
+print(first_name[1])
+print(first_name[2])
+print(first_name[3])
+
+print(first_name[-1])
+print(first_name[-2])
+print(first_name[-3])
+print(first_name[-4])
+
+print(first_name[0:3])
+print(first_name[0:4])
+print(first_name[1:4])
+
+full_name = first_name+"Kumar"+last_name
+
+print(full_name[0:400])
+
+print(full_name[0::2])
+
+print(full_name[::-2])
+
+print(full_name[:1:-2])
+
+
+
